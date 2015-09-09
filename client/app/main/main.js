a2ad4f9f298637f41731a19d0486697311108716
Jérémy Morin <jer.morin@free.fr>
2015-09-09 17:19:31 +0200

    feat(init) using angular-fullstack generator

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('easyStockApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
